d7b7148d28fe8cbd6e348667443fd5922c166ae9
mlefter <m.lefter@lumc.nl>
2019-10-15 11:43:48 +0200

    Refactor schema validation: variable names; hard constraint on id and optional location.

--- retriever/schema_validation.py
+++ retriever/schema_validation.py
@@ -1,18 +1,18 @@
 from schema import Schema, And, Optional
 
-feature_point = Schema({'type': 'point',
+point = Schema({'type': 'point',
                         'position': And(int, lambda n: n >= 0),
-                        Optional('uncertain'): And(bool, True)})
+                Optional('uncertain'): And(bool, True)})
 
 location = Schema({'type': str,
-                   'start': feature_point,
-                   'end': feature_point,
+                   'start': point,
+                   'end': point,
                    Optional('strand'): int})
 
-feature = Schema({'type': And(str, len),
-                  Optional('id'): str,
-                  Optional('features'): list,
+feature = Schema({'type': str,
+                  'id': str,
                   Optional('location'): location,
+                  Optional('features'): list,
                   Optional('qualifiers'): dict})
 
 
